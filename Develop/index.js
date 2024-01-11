628b912e9ba4e47c6ae1be0ff32a8e005a4e9cb3
Corey Donahue <cdartswebdev@gmail.com>
2024-01-11 11:15:20 -0700

    Updated files added README

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,103 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const path = require("path");
+// const util = require('util');\const api = require('./utils/api.js');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+// Generate the HTML string???
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const name = "My Blog Post";
+const content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 
+const htmlString = `
+  <div>
+    <h1>${name}</h1>
+    <p>${content}</p>
+  </div>
+`;
+
+console.log(htmlString);
+
+// Check the end of last nights video for the correct way to do the above for the html creation then adjust for a read me file
+// Array copied from github.com/dopecello 
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "Please name your Project.",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please describe the purpose and functionality of this project.",
+  },
+  {
+    type: "input",
+    name: "screenshot",
+    message: "Please provide the relative path to the image you want to use as the screenshot."
+  },
+  {
+    type: "input",
+    name: "link",
+    message: "Please provide a URL where a user can access your deployed application."
+  },
+  {
+    type: "checkbox",
+    name: "license",
+    message: "Please select a license applicable to this project.",
+    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+  },
+  {
+    type: "input",
+    name: "require",
+    message: "List any project dependencies here.",
+  },
+  {
+    type: "input",
+    name: "features",
+    message: "List some cool features about this project here.",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message:
+      "State the languages or technologies associated with this project.",
+  },
+  {
+    type: "input",
+    name: "creator",
+    message: "Write your GitHub username.",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Provide a valid email address.",
+  },
+  {
+    type: "input",
+    name: "contributors",
+    message: "Please list any contributors. (Use GitHub usernames)",
+    default: "",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "Provide walkthrough of required tests if applicable.",
+  },
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  
+}
+inquirer.prompt(questions).then((answers) => {
+  // Write the answers to a new file
+  fs.writeFile("userInput.json", JSON.stringify(answers, null, 2), (err) => {
+    if (err) throw err;
+    console.log("Data written to file");
+  });
+  // TODO: Create a function to initialize app
+  function init() {}
 
-// TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+  // Function call to initialize app
+  init();
+});
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,65 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "none") {
+    return `[Github license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return "";
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+   if (license) {
+    return `https://github.com/licenses/${license}`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+   if (license) {
+    return `
+## License
+
+This project is licensed under the ${license} license. Click [here](${getLicenseLink(license)}) for more information.
+`;
+  } else {
+    return '';
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
-
+  return `# ${data.title} 
+${renderLicenseBadge(data.license)}
+## Description
+${data.description}
+## Deployed Application URL
+${data.link}
+## Screenshot
+![alt-text:'Screen shot of app'](${data.screenshot})
+## Table of Contents
+* [Features](#features)
+* [Languages & Dependencies](#languagesanddependencies)
+* [How to Use This Application](#HowtoUseThisApplication)
+* [Contributors](#contributors)
+* [Testing](#testing)
+* [Questions](#questions)
+  ## Features
+  ${data.features}
+  ## Languages & Dependencies
+  ${data.require}
+  ## How to Use This Application
+  ${data.usage}
+  ## Contributors
+  ${data.contributors}
+  ## Testing
+  ${data.test}
+  ## Questions
+  Please send your questions [here](mailto:${data.email}?subject=[GitHub]%20Dev%20Connect) or visit [github/${data.creator}](https://github.com/${data.creator}).
 `;
 }
 
